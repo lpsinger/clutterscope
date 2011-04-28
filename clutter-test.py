2d356cfe6e1897228b5af269dea834cab4361647
Leo Singer <leo.singer@ligo.org>
2011-04-28 13:42:17 -0700

    Fooling around with constraints

--- clutter-test.py
+++ clutter-test.py
@@ -8,77 +8,114 @@ from clutter import cogl
 
 stage = clutter.Stage()
 
-bgcolor = clutter.Color(40, 40, 40, 255)
-grcolor = clutter.Color(60, 60, 60, 255)
+
+class ClutterScope(clutter.Actor):
+	"""A Clutter-powered digital storage oscilloscope."""
+	__gtype_name__ = 'ClutterScope'
+
+
+def hline(y, x1, x2):
+	"""Draw a horizontal line from (x1, y) to (x2, y)."""
+	cogl.path_line(x1, y, x2, y)
+
+
+def vline(x, y1, y2):
+	"""Draw a vertical line from (x, y1) to (x, y2)."""
+	cogl.path_line(x, y1, x, y2)
+
 
 class Graticule(clutter.Actor):
-	"""Draw grid."""
+	"""Actor that provides grid and axes for a ClutterScope.
+	This actor paints into an area that extends from half its width above its
+	top to half its width above its bottom, so that it is possible to conveniently
+	parent objects to it and have their coordinates referenced from (0, 0) refer
+	to the center of the stage."""
 	__gtype_name__ = 'Graticule'
 
-	def do_paint(self):
-		half_w = 0.5 * self.get_width()
-		half_h = 0.5 * self.get_height()
-		half = long(math.ceil(max(half_w, half_h)))
-		cogl.set_source_color(grcolor)
-		cogl.push_matrix()
-		cogl.translate(half_w, half_h, 0)
-		for j in range(2):
-			for i in range(2):
-				for x in range(0, half, 60):
-					cogl.path_line(x, -half, x, half)
-				for x in range(0, half, 6):
-					cogl.path_line(x, 0, x, -4)
-				for x in range(0, half, 30):
-					cogl.path_line(x, 0, x, -8)
-				cogl.path_stroke()
-				cogl.scale(-1, 1, 1)
-			cogl.rotate(90, 0, 0, 1)
-		cogl.pop_matrix()
+	"""Pixels between major gridlines"""
+	MAJOR_PIXELS = 60
 
-class Trace(clutter.Rectangle):
+	"""Background color"""
+	BACKGROUND_COLOR = clutter.Color(40, 40, 40, 255)
+
+	"""Gridline color"""
+	GRIDLINE_COLOR = clutter.Color(60, 60, 60, 255)
+
+	def __init__(self):
+		super(Graticule, self).__init__()
+		self.__bind_constraint = None
+		self.__align_x_constraint = None
+
+	def do_paint(self):
+		"""Actor->paint signal handler."""
+		w = self.get_width()
+		h = self.get_height()
+		half_w = 0.5 * w
+		half_h = 0.5 * h
+		half_major = self.MAJOR_PIXELS / 2
+		tenth_major = self.MAJOR_PIXELS / 10
+
+		# Fill background.
+		cogl.set_source_color(self.BACKGROUND_COLOR)
+		cogl.rectangle(-half_w, -half_h, half_w, half_h)
+
+		# Create paths for vertical gridlines.
+		x0 = int((-half_w // self.MAJOR_PIXELS) * self.MAJOR_PIXELS)
+		for x in xrange(x0, int(math.ceil(half_w)), self.MAJOR_PIXELS):
+			halfway = x + half_major
+			vline(x, -half_h, half_h)
+			vline(halfway, -8, 0)
+			for xx in xrange(x + tenth_major, halfway, tenth_major):
+				vline(xx, -4, 0)
+			for xx in xrange(halfway + tenth_major, x + self.MAJOR_PIXELS, tenth_major):
+				vline(xx, -4, 0)
+
+		# Create paths for horizontal gridlines.
+		y0 = int((-half_h // self.MAJOR_PIXELS) * self.MAJOR_PIXELS)
+		for y in xrange(y0, int(math.ceil(half_h)), self.MAJOR_PIXELS):
+			halfway = y + half_major
+			hline(y, -half_w, half_w)
+			hline(halfway, 0, 8)
+			for yy in xrange(y + tenth_major, halfway, tenth_major):
+				hline(yy, 0, 4)
+			for yy in xrange(halfway + tenth_major, y + self.MAJOR_PIXELS, tenth_major):
+				hline(yy, 0, 4)
+
+		# Stroke gridlines.
+		cogl.set_source_color(self.GRIDLINE_COLOR)
+		cogl.path_stroke()
+
+	def do_parent_set(self, old_parent):
+		"""Actor->parent_set signal handler."""
+		parent = self.get_parent()
+		if self.__bind_constraint:
+			self.remove_constraint(self.__bind_constraint)
+		if parent:
+			self.__bind_constraint = clutter.BindConstraint(parent, clutter.BIND_SIZE, 0.)
+			self.add_constraint(self.__bind_constraint)
+		else:
+			self.__bind_constraint = None
+		if self.__align_x_constraint:
+			self.remove_constraint(self.__align_x_constraint)
+		if parent:
+			self.__align_x_constraint = clutter.AlignConstraint(parent, clutter.ALIGN_X_AXIS, 0.)
+			self.add_constraint(self.__align_x_constraint)
+		else:
+			self.__align_x_constraint = None
+
+
+class Trace(clutter.Actor):
 	__gtype_name__ = 'Trace'
 
 	def __init__(self):
 		super(Trace, self).__init__()
-		self.set_anchor_point(0.5 * self.get_width(), 0.5 * self.get_height())
-		#self.set_anchor_point_from_gravity(clutter.GRAVITY_CENTER)
+		#self.set_anchor_point(0.5 * self.get_width(), 0.5 * self.get_height())
+		self.set_anchor_point_from_gravity(clutter.GRAVITY_CENTER)
 		#self.set_property('scale-gravity', 'center')
 		self.color = clutter.color_from_string('cyan')
 
-		drag = clutter.DragAction()
-		drag.set_drag_axis(clutter.DRAG_X_AXIS)
-		drag.set_drag_threshold(24, gobject.G_MAXUINT)
-		self.add_action(drag)
-
-		drag = clutter.DragAction()
-		drag.set_drag_axis(clutter.DRAG_Y_AXIS)
-		drag.set_drag_threshold(gobject.G_MAXUINT, 24)
-
-		self.last_time = 0
-		self.add_action(drag)
-		self.connect_after('scroll-event', self.scroll)
-		self.set_reactive(True)
-
-	@staticmethod
-	def scroll(self, event):
-		if event.time - self.last_time > 100:
-			direction = event.direction
-			x, y = self.get_scale()
-			if direction == clutter.SCROLL_UP:
-				self.set_scale(x, 2 * y)
-			elif direction == clutter.SCROLL_DOWN:
-				if y > 1. / 4:
-					self.set_scale(x, 0.5 * y)
-			elif direction == clutter.SCROLL_LEFT:
-				self.set_scale(2 * x, y)
-			elif direction == clutter.SCROLL_RIGHT:
-				if x > 1. / 4:
-					self.set_scale(0.5 * x, y)
-			self.last_time = event.time
-
 	def do_paint(self):
 		x = numpy.arange(800)
-		#y = 20*numpy.random.randn(len(x))
 		y = 20 * numpy.sin(x * 0.1)
 
 		# Plot trace, setting down lines wherever both x and y are finite
@@ -97,16 +134,18 @@ class Trace(clutter.Rectangle):
 		cogl.path_stroke()
 
 stage.set_size(576, 576)
-stage.set_color(bgcolor)
 
 gr = Graticule()
-tr = Trace()
-tr.set_size(*stage.get_size())
-tr.set_position(0, 576/2)
+
+#tr = Trace()
+#tr.set_size(*stage.get_size())
+#tr.set_position(0, 576/2)
 stage.add(gr)
-stage.add(tr)
-tr.set_reactive(True)
-gr.add_constraint(clutter.BindConstraint(stage, clutter.BIND_SIZE | clutter.BIND_POSITION, 0))
+gr.set_size(*stage.get_size())
+gr.set_position(576/2, 576/2)
+#stage.add(tr)
+#tr.set_reactive(True)
+#gr.add_constraint(clutter.BindConstraint(stage, clutter.BIND_SIZE | clutter.BIND_POSITION, 0))
 #tr.add_constraint(clutter.BindConstraint(stage, clutter.BIND_SIZE, 0))
 
 stage.connect('destroy', clutter.main_quit)
